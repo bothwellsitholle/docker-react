e2d5bd7fb646b83afdd0a24b666e623fb81ea625
Bothwell <bothwell@deepalert.ai>
2021-09-05 00:21:20 +0200

    aws key

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Bothwell Rich!
+          Hi Bothwell Rich!!
         </p>
         <a
           className="App-link"
