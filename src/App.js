c83975e359a3c0277e23bfa8b0b601793a8bb246
Bothwell <bothwell@deepalert.ai>
2021-09-07 11:56:08 +0200

    richman

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Bothwell Richman!
+          Hi Bothwell Richman!!
         </p>
         <a
           className="App-link"
