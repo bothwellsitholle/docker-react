f179220090e9ccea14b0a40b185270029af6920d
Bothwell <bothwell@deepalert.ai>
2021-09-07 14:32:23 +0200

    feature commit5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Feature branch2
+          Feature branch!
         </p>
         <a
           className="App-link"
